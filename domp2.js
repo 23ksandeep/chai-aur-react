96cbe11089fa16db3f7efdb79c6c824c2a52c32a
23ksandeep <132960171+23ksandeep@users.noreply.github.com>
2024-01-26 00:21:37 +0530

    Add files via upload

---
+++ domp2.js
@@ -0,0 +1,6 @@
+var co = document.querySelector("#container")
+var love = document.querySelector("i")
+
+con.addEventListener("dbclick",function(){
+    love.style.transform = "translate(-50%,-50%) scale(1)"
+})
